c19ed3b2dfb3f9911cc38431c0a159fb36ea3764
supercodernoshadow <dadebonojo@icloud.com>
2023-09-25 22:43:43 -0600

    deploy amm and tokens

---
+++ test/AMM.js
@@ -0,0 +1,43 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+describe('AMM', () => {
+
+  let accounts,
+      deployer
+
+  let token1,
+      token2,
+      amm
+
+  beforeEach(async () => {
+    // Setup Accounts
+    accounts = await ethers.getSigners()
+    deployer = accounts[0]
+
+    // Deploy Token
+    const Token = await ethers.getContractFactory('Token')
+    token1 = await Token.deploy('Dapp University', 'DAPP', '1000000') // 1 Million Tokens
+    token2 = await Token.deploy('USD Token', 'USD', '1000000') // 1 Million Tokens
+
+    // Deploy AMM
+    const AMM = await ethers.getContractFactory('AMM')
+    amm = await AMM.deploy(token1.address, token2.address)
+  })
+
+  describe('Deployment', () => {
+
+    it('has an address', async () => {
+      expect(amm.address).to.not.equal(0x0)
+    })
+
+    it('tracks token1 address', async () => {
+      expect(await amm.token1()).to.equal(token1.address)
+    })
+
+    it('tracks token2 address', async () => {
+      expect(await amm.token2()).to.equal(token2.address)
+    })
+
+  })
+})
